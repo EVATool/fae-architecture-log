4ff179895f22405e50224c6162232c1eaea54817
MHallweger <marvinhallweger@t-online.de>
2021-01-15 16:10:32 +0100

    initialized project

---
+++ EVATool/src/main/java/com/FAE/EVATool/EvaToolApplication.java
@@ -0,0 +1,13 @@
+package com.FAE.EVATool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaToolApplication.class, args);
+	}
+
+}
